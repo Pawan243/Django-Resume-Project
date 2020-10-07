60cc09adfab00c281f76b5982ce0f2509c2bf990
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-10-07 23:43:05 +0530

    modified

--- resume/edu/views.py
+++ resume/edu/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def skill(request):
-
-    return render(request, 'edu/skill.html')
+    context = {'skill': 'active'}
+    return render(request, 'edu/skill.html',context)

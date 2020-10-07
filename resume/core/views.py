70499176097e01526db956ef22f3131f0e3cb9ec
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-10-07 23:43:22 +0530

    modified

--- resume/core/views.py
+++ resume/core/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-
-    return render(request, 'core/home.html')
+    context = {'home': 'active'}
+    return render(request, 'core/home.html', context)
 
 def contact(request):
-    
-    return render(request, 'core/contact.html')
+    context = {'contact': 'active'}
+    return render(request, 'core/contact.html', context)
